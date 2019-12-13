d16029fd33c66055d0cb7e3d0dfa7ff4ee40c70e
sdeibel1 <deibel.sebastian@gmail.com>
2019-12-13 14:38:18 -0600

    MCTS hopefully finished, needs documentation

--- spirecomm/ai/mcts.py
+++ spirecomm/ai/mcts.py
@@ -3,99 +3,69 @@ import operator
 from spirecomm.ai.combatSim import CombatSim
 
 
+def backpropagate(node, result):
+    """Backpropagates a result of a simulation up the tree."""
+    n = node
+    while n:
+        n.increment(result)
+        n = n.parent
+
+
 class MCTS:
     def __init__(self, combat_state):
         self.sim = CombatSim(combat_state)
-        self.root_state = combat_state
-        root = Tree(self.sim.get_state())
-        self.nodes = []
-        self.expand(root)
+        root = Tree(combat_state)
 
+    def get_action(self, n=100):
+        """Returns the best action after running n rounds of MCTS."""
+        self.build_tree(n)
+        best = max(self.root.children, key=operator.attrgetter("value"))
+        return best.card, best.target
 
     def build_tree(self, n):
+        """Runs the process of selection, expansion, rollout, backpropagation n times."""
         for i in range(n):
             node_to_expand = self.select()
             rollout_node = self.expand(node_to_expand)
             result = self.rollout(rollout_node)
-            self.backpropagate(rollout_node, result)
-
-
-
-    def get_possible_actions(self, node):
-        actions = set()
-        sim = CombatSim(node.state)
-        for c in sim.hand:
-            if c.has_target:
-                for t in sim.monsters:
-                    actions.add((c, t))
-            else:
-                actions.add(c, sim.player)
-        return actions
+            backpropagate(rollout_node, result)
 
     # node is the root node at which we start, recursively goes down and selects
     def select(self, node):
-        if self.fully_expanded(node):
+        """Recursively selects a node for expansion."""
+        if node.fully_expanded():
             max_child = max(node.children, key=operator.attrgetter("value"))
             return self.select(max_child)
         else:
             return node
 
-    def fully_expanded(self, node):
-        expanded = {(n.card, n.target) for n in node.children}
-        possible = self.get_possible_actions(node)
+    def expand(self, node):
+        """Adds a node to the tree if the node for expansion is not fully expanded.
+        Otherwise, returns the node for expansion.
 
-        return False if (possible - expanded) else True
+        Args:
+            node -- the node to expand.
+
+        Returns:
+            Tree. the node to be simulated next.
+        """
+        self.sim.change_state(node.state)
+        self.sim.play(node.card, node.target)
+        if self.sim.is_over():
+            return node
+        else:
+            expanded = {(n.card, n.target) for n in node.children}
+            possible = node.get_possible_actions()
+            action_to_expand = (possible - expanded).sample(1)
+            child = Tree(self.sim.get_state(), action_to_expand)
+            node.add_child(child)
+            return child
 
     def rollout(self, node):
         self.sim.change_state(node.state)
         result = self.sim.sim_combat(node.card, node.target)
+        return result
 
-
-
-    def expand(self, node):
-        sim = CombatSim(node.state)
-        sim.play(node.card, node.target)
-        if sim.is_over():
-            return node
-        else:
-            expanded = {(n.card, n.target) for n in node.children}
-            possible = self.get_possible_actions(node)
-            action_to_expand = (possible - expanded).sample(1)
-            sim.change_state(node.state)
-            sim.play(action_to_expand)
-            node_to_expand = Tree(sim)
-
-
-        ancestors = []
-        parent = node.parent
-        while parent:
-            ancestors.append(parent)
-        for a in ancestors.reverse():
-            if a.card is not None:
-                self.sim.play(a.card, a.target)
-
-        expanded = {(n.card, n.target) for n in node.children}
-        possible = self.get_possible_actions(node)
-
-        if (possible - expanded):
-            node_to_expand = (possible - expanded).sample(1)
-        else:
-            node_to_expand =
-
-
-        for c in self.sim.hand:
-            if c.has_target:
-                for t in self.sim.monsters:
-                    if (c, t) not in set(map(lambda n: (n.card, n.target), node.children)):
-                    result = self.sim.sim_combat(c, t)
-                    new_node = Tree(c, t)
-                    node.add_child(new_node, result)
-                    self.nodes.append(new_node)
-            else:
-                result = self.sim.sim_combat(c, self.sim.player)
-                new_node = Tree(c, t)
-                node.add_child(new_node, result)
-                self.nodes.append(node)
 
 class Tree:
     def __init__(self, state, card=None, target=None):
@@ -110,12 +80,10 @@ class Tree:
     def add_child(self, node, result):
         self.children.append(node)
         node.parent = self
-        node.increment(result)
-        node.backpropagate(result)
 
     def increment(self, result):
         self.wins += result
-        self.total += result
+        self.total += 1
         self.value = self.calc_value()
 
     def calc_value(self):
@@ -124,8 +92,18 @@ class Tree:
 
         return ave_reward + math.sqrt(2)*(math.sqrt(math.log(parent_sims)/self.total))
 
-    def backpropagate(self, result):
-        p = self.parent
-        while p:
-            p.increment()
-            p = p.parent
+    def fully_expanded(self):
+        expanded = {(n.card, n.target) for n in self.children}
+        possible = self.get_possible_actions()
+
+        return False if (possible - expanded) else True
+
+    def get_possible_actions(self):
+        actions = set()
+        for c in self.state['hand']:
+            if c.has_target:
+                for t in self.state['monsters']:
+                    actions.add((c, t))
+            else:
+                actions.add((c, self.state['player']))
+        return actions
